79c53f69d185e76e25b37da5f4a1cf624c92e995
Shalem <shalem.raz@post.runi.ac.il>
2024-01-01 19:06:22 +0200

    Homework 2

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,14 +5,13 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
 		for (int i = 0; i < n; i++) {
-			System.out.println("");
 			if (i % 2 == 1) {
-				System.out.print("  ");
+				System.out.print(" ");
 			}
 			for (int j = 0; j < n; j++) {
 				System.out.print(" * ");
 			}
+			System.out.println("");
 		}
-		System.out.println("");
 	}
 }
